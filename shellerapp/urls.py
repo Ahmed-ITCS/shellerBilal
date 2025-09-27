8882d564c4da4583405c9d1767148d82161d38a6
Ahmed-ITCS <ahmedkhawarbs@gmail.com>
2025-09-27 15:19:42 +0500

    allowed

--- shellerapp/urls.py
+++ shellerapp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView,SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +11,5 @@ urlpatterns = [
 
     # ReDoc UI (works in Django 5.x without templates)
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
