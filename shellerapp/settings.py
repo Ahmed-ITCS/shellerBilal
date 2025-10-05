a9c00d33ec310526c6b6f8450b5a5dfb0c9b73d9
Ahmed-ITCS <ahmedkhawarbs@gmail.com>
2025-10-05 17:45:31 +0500

    allowed

--- shellerapp/settings.py
+++ shellerapp/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = 'django-insecure-yz(co7z2l@v!!ub2thdg&4%t#j=r$3wo($m#%pisrf$y1xe%$5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['gohesi.pythonanywhere.com']
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
