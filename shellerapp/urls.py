2a519d8ccaf558fee66a1ec32f52b64be24e850c
Ahmed-ITCS <ahmedkhawarbs@gmail.com>
2025-10-08 01:34:34 +0500

    fixed

--- shellerapp/urls.py
+++ shellerapp/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     
     # OpenAPI schema endpoints
     path('openapi.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('static/swagger-ui.html/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
